4fee74adec173ffacf4f38d14dae0e79deec904e
Partho <parthobhai61@gmail.com>
2026-08-07 13:34:49 +0600

    Update dashboard.py

--- python/dashboard.py
+++ python/dashboard.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 import subprocess
 
-def toggle_light():
-    subprocess.run(["./cpp/main"])   # C++ compiled binary run 
-    print("Light toggled via C++ backend")
+def run_backend():
+    subprocess.run(["./cpp/main"])
 
 root = tk.Tk()
 root.title("Smart Home Dashboard")
 
-btn_light = tk.Button(root, text="Toggle Light", command=toggle_light)
+# Buttons for devices
+btn_light = tk.Button(root, text="Toggle Light", command=run_backend)
 btn_light.pack(pady=10)
 
-root.mainloop()
+btn_fan = tk.Button(root, text="Toggle Fan", command=run_backend)
+btn_fan.pack(pady=10)
 
+btn_ac = tk.Button(root, text="Toggle AC", command=run_backend)
+btn_ac.pack(pady=10)
+
+btn_door = tk.Button(root, text="Toggle DoorLock", command=run_backend)
+btn_door.pack(pady=10)
+
+root.mainloop()
